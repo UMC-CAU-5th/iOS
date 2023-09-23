cb82d585c05d7f71a2e607a10b350dd423b8ea03
JEONG-J <ikejhc159@gmail.com>
2023-09-24 01:45:48 +0900

    Feat : 1st_mission

---
+++ jeong/1st/1st/CouponBtn.swift
@@ -0,0 +1,20 @@
+//
+//  CouponBtn.swift
+//  1st
+//
+//  Created by 정의찬 on 2023/09/23.
+//
+
+import UIKit
+
+class CouponBtn: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
