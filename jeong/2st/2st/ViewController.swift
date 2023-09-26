1bb4adff03360ba2ac3662575f3cbc4805280a3d
JEONG-J <ikejhc159@gmail.com>
2023-09-26 21:27:29 +0900

    Feat : 2주차_당근마켓 코드베이스

--- jeong/2st/2st/ViewController.swift
+++ jeong/2st/2st/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        makeConstraints()
     }
+    
+    private lazy var topView : TopStatusView = {
+        let view = TopStatusView()
+        return view
+    }()
+    
+    private lazy var bottomTabBar : BottomTabBar = {
+        let tab = BottomTabBar()
+        
+        let tabAppear = UITabBarAppearance()
+        tabAppear.stackedLayoutAppearance.selected.iconColor = UIColor.orange
+        tabAppear.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.orange]
+        tabAppear.stackedLayoutAppearance.selected.badgeBackgroundColor = UIColor.orange
+        tabAppear.stackedLayoutAppearance.normal.badgeBackgroundColor = UIColor.orange
+        
+        tab.standardAppearance = tabAppear
+        tab.backgroundColor = .white
+        return tab
+    }()
 
+    
+    private func makeConstraints(){
+        [topView, bottomTabBar].forEach{ self.view.addSubview($0) }
+        
+        topView.snp.makeConstraints{ (make) -> Void in
+            make.top.equalToSuperview()
+            make.right.equalToSuperview()
+            make.left.equalToSuperview()
+        }
+        
+        bottomTabBar.snp.makeConstraints{ (make) -> Void in
+            //make.top.equalTo(pass) ->> 상단 테이블뷰와 맞추기
+            make.height.equalTo(90)
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
 
 }
 
