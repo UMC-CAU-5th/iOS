9944f9b1a3b1b84c2b613a9f27999c04e31c1311
신지연 <shinjy@sinjiyeon-ui-MacBookAir.local>
2023-10-01 15:51:02 +0900

    2주차 미션

---
+++ 제이/2주차/UMC_2주차_미션/UMC_2주차_미션/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  UMC_2주차_미션
+//
+//  Created by 신지연 on 2023/10/01.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+}
+
