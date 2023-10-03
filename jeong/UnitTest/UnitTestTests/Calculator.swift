38921cd9b2dbe9682029efd6aad69ea8f0b45689
JEONG-J <ikejhc159@gmail.com>
2023-10-04 08:47:47 +0900

    Feat : UnitTestCode

---
+++ jeong/UnitTest/UnitTestTests/Calculator.swift
@@ -0,0 +1,18 @@
+//
+//  Calculator.swift
+//  UnitTest
+//
+//  Created by 정의찬 on 10/4/23.
+//
+
+import Foundation
+
+class Calculator{
+    func add(_ a: Int, _ b: Int) -> Int{
+        return a + b
+    }
+    
+    func sub(_ a:Int, _ b: Int) -> Int{
+        return a - b
+    }
+}
