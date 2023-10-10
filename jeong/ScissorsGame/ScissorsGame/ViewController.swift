86ef4232d361596693fa634e31b03370852201b9
JEONG-J <ikejhc159@gmail.com>
2023-10-10 13:14:42 +0900

    Feat : 4주차 실습

--- jeong/ScissorsGame/ScissorsGame/ViewController.swift
+++ jeong/ScissorsGame/ScissorsGame/ViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let nameData = ["1.png", "2.png", "3.png"]
+    let nameData: [InputType] = [.scissors, .paper, .rock]
+    
+    enum InputType: String {
+        case scissors
+        case paper
+        case rock
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +25,27 @@ class ViewController: UIViewController {
         paperBtn.addTarget(self, action: #selector(didTapBtn2), for: .touchUpInside)
         rockBtn.addTarget(self, action: #selector(didTapBtn3), for: .touchUpInside)
     }
+    
+    private lazy var resultLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "결과!!"
+        return label
+    }()
+    
+    private lazy var userLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "user"
+        return label
+    }()
+    
+    private lazy var computerLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "computer"
+        return label
+    }()
     
     private lazy var stackView: UIStackView = {
         let stack = UIStackView()
@@ -30,7 +57,7 @@ class ViewController: UIViewController {
     
     private lazy var computerView: UIImageView = {
         let image = UIImageView()
-        image.image = UIImage(named: "2.png")
+        image.image = UIImage(named: "scissors.png")
         image.contentMode = .scaleAspectFit
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
@@ -38,7 +65,7 @@ class ViewController: UIViewController {
     
     private lazy var myView: UIImageView = {
         let image = UIImageView()
-        image.image = UIImage(named: "2.png")
+        image.image = UIImage(named: "scissors.png")
         image.contentMode = .scaleAspectFit
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
@@ -83,11 +110,23 @@ class ViewController: UIViewController {
         view.addSubview(stackView)
         stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
         stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-        stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 130).isActive = true
         stackView.heightAnchor.constraint(equalToConstant: 300).isActive = true
         
-        stackView.addArrangedSubview(computerView)
         stackView.addArrangedSubview(myView)
+        stackView.addArrangedSubview(computerView)
+        
+        view.addSubview(resultLabel)
+        resultLabel.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 20).isActive = true
+        resultLabel.centerXAnchor.constraint(equalTo: stackView.centerXAnchor).isActive = true
+        
+        view.addSubview(userLabel)
+        userLabel.bottomAnchor.constraint(equalTo: stackView.topAnchor, constant: 10).isActive = true
+        userLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 30).isActive = true
+        
+        view.addSubview(computerLabel)
+        computerLabel.bottomAnchor.constraint(equalTo: stackView.topAnchor, constant: 10).isActive = true
+        computerLabel.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: -30).isActive = true
         
         view.addSubview(inputStack)
         inputStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
@@ -101,21 +140,63 @@ class ViewController: UIViewController {
     }
     
     @objc func didTapBtn1(){
-        myView.image = UIImage(named: "1")
+        myView.image = UIImage(named: "scissors.png")
         let randomInput = nameData.randomElement()!
-        computerView.image = UIImage(named: randomInput)
+        computerView.image = UIImage(named: randomInput.rawValue)
+        
+        resultLabel.text = result(myInput: .scissors, computerInput: randomInput) // 게임의 승부 계산 로직
+         
     }
     
     @objc func didTapBtn2(){
-        myView.image = UIImage(named: "2")
+        myView.image = UIImage(named: "paper.png")
         let randomInput = nameData.randomElement()!
-        computerView.image = UIImage(named: randomInput)
+        computerView.image = UIImage(named: randomInput.rawValue)
+        
+        resultLabel.text = result(myInput: .paper, computerInput: randomInput) // 게임의 승부 계산 로직
     }
     
     @objc func didTapBtn3(){
-        myView.image = UIImage(named: "3")
+        myView.image = UIImage(named: "rock.png")
         let randomInput = nameData.randomElement()!
-        computerView.image = UIImage(named: randomInput)
+        computerView.image = UIImage(named: randomInput.rawValue)
+        
+        resultLabel.text = result(myInput: .rock, computerInput: randomInput) // 게임의 승부 계산 로직
+    }
+    
+    private func result(myInput: InputType, computerInput : InputType) -> String{
+        switch myInput {
+        case .scissors:
+            switch computerInput {
+            case .scissors:
+                return "무승부"
+            case .rock:
+                return "패배"
+            case .paper:
+                return "승리"
+            }
+            
+        case .paper:
+            switch computerInput {
+            case .scissors:
+                return "패배"
+            case .rock:
+                return "승리"
+            case .paper:
+                return "무승부"
+            }
+            
+        case .rock:
+            switch computerInput {
+            case .scissors:
+                return "승리"
+            case .rock:
+                return "무승부"
+            case .paper:
+                return "패배"
+            }
+        
+        }
     }
 }
 
